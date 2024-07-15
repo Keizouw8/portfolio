9623ab99e7947eb26455106af9d36ec7ae543738
keizouw <keizouw8@gmail.com>
2024-07-14 19:54:52 -0700

    optimize production server

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const ejs = require("ejs");
 
 const PORT = process.env.PORT || 3030;
 const app = express();
 
-app.set("view engine", "ejs");
 app.use(cors());
 app.use("/static", express.static("static"));
 
-app.get("/", (_, res) => res.render("main"));
+ejs.renderFile("./views/main.ejs", function(err, str){
+    if(err) process.exit();
+    app.get("/", function(_, res){
+        res.type("html").send(str);
+    });
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
