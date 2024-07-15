9c9487aca95fbd14a540c251ca28efa9b267db35
keizouw <keizouw8@gmail.com>
2024-07-14 20:10:14 -0700

    lifted event listener cap

--- dev.js
+++ dev.js
@@ -11,6 +11,8 @@ const app = express();
 fs.watch("./views/", { recursive: true }, () => eventEmitter.emit("change"));
 fs.watch("./static/", { recursive: true }, () => eventEmitter.emit("change"));
 
+eventEmitter.setMaxListeners(0);
+
 app.set("view engine", "ejs");
 app.use(cors());
 app.use("/static", express.static("static"));
