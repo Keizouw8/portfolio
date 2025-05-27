b91af67baca5e95bb0ec497be4d2efbbfd6dbcbd
Keizou Wang <keizouw8@gmail.com>
2025-05-27 11:26:39 -0700

    animate frills

--- src/lib/frills.svelte
+++ src/lib/frills.svelte
@@ -1,6 +1,6 @@
-<svelte:window bind:innerHeight bind:innerWidth />
+<svelte:window bind:innerHeight bind:innerWidth on:scroll={scroll} />
 <div class="fullpage">
-	<div class="top" style="background: {color}"></div>
+	<div class="top" bind:this={top} style="background: {color}; transform: translateX({100 * topTranslate}vw)"></div>
 	<div class="points t">
 			{#each { length } }
 			<div class="point" style="background: {background}"></div>
@@ -9,7 +9,7 @@
 	<div class="container" style="background: {background}">
 			{@render children?.()}
 	</div>
-	<div class="bottom" style="background: {color}"></div>
+	<div class="bottom" bind:this={bottom} style="background: {color}; transform: translateX({100 * bottomTranslate}vw)"></div>
 	<div class="points b">
 			{#each { length } }
 			<div class="point" style="background: {background}"></div>
@@ -21,37 +21,29 @@
 	let innerHeight = $state(0);
 	let innerWidth = $state(0);
 	let length = $derived(Math.ceil(innerWidth/(innerHeight*0.12))+1);
+	let top: HTMLDivElement = $state() as HTMLDivElement;
+	let bottom: HTMLDivElement = $state() as HTMLDivElement;
+	let topRect: DOMRect = $state({ y: 0, height: 0 }) as DOMRect;
+	let bottomRect: DOMRect = $state({ y: 0, height: 0, bottom: 0 }) as DOMRect;
+	let topTranslate = $derived(topRect.y < 0 ? topRect.y / topRect.height : Math.max(1 - (innerHeight - topRect.y) / topRect.height / 5, 0));
+	let bottomTranslate = $derived((innerHeight - bottomRect.y) / bottomRect.height / (bottomRect.bottom < innerHeight ? 5 : 1) - (bottomRect.bottom < innerHeight ? 0.2 : 1));
+	function scroll(){
+		topRect = top.getBoundingClientRect();
+		bottomRect = bottom.getBoundingClientRect();
+	}
 </script>
 <style>
 	.fullpage{
 		z-index: unset;
 	}
-	
+
 	.container{
-		z-index: 1;
+		z-index: 2;
 		position: absolute;
 		width: 100vw;
 		height: 66vh;
 		left: 0;
 		top: 17vh;
-	}
-	
-	@keyframes top-in{
-		from{
-			left: 100vw;
-		}
-		to{
-			left: 0;
-		}
-	}
-	
-	@keyframes bottom-in{
-		from{
-			right: 100vw;
-		}
-		to{
-			right: 0;
-		}
 	}
 
 	.top, .bottom{
@@ -63,14 +55,10 @@
 
 	.top{
 		top: 0;
-		animation: linear top-in;
-		animation-timeline: scroll();
 	}
 
 	.bottom{
 		bottom: 0;
-		animation: linear bottom-in;
-		animation-timeline: scroll();
 	}
 
 	.points{
@@ -82,22 +70,20 @@
 	}
 
 	.points.t{
-		top: 15vh;
+		top: calc(15vh + 1px);
 		animation: linear scroll-left 4s infinite;
 	}
 
 	.points.b{
-		bottom: 15vh;
+		bottom: calc(15vh + 1px);
 		animation: linear scroll-right 4s infinite;
 	}
-	
+
 	.t .point{
-		top: 1px;
 		clip-path: polygon(50% 0, 0% 100%, 100% 100%);
 	}
-	
+
 	.b .point{
-		top: -1px;
 		clip-path: polygon(50% 100%, 0 0, 100% 0);
 	}
 
